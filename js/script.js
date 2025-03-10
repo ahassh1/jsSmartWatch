728b6ad830c3ebbac3ca142e53ece1b08b7fe86a
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-03-10 18:05:23 +0600

    size section border in function useage

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
-const ringButton = document.querySelectorAll('.ring-button');
-
-for(let i = 0; i < ringButton.length; i++){
-    const ringBtn = ringButton[i]
-    ringBtn.addEventListener('click', function(event){
-        for(let j= 0; j < ringButton.length; j++){
-            ringButton[j].classList.remove('border-purple-600');
-            ringButton[j].classList.add('border-gray-700');
+const ringButtons = document.querySelectorAll(".ring-button");
+for (let i = 0; i < ringButtons.length; i++) {
+    const ringBtn = ringButtons[i];
+    ringBtn.addEventListener("click", function (event) {
+        const color = event.target.id.replace("-color", "");
+        console.log(color)
+        for (let j = 0; j < ringButtons.length; j++) {
+            ringButtons[j].classList.remove("border-purple-600");
+            ringButtons[j].classList.add("border-gray-300");
         }
-
-        event.target.classList.add('border-purple-600'); 
-        event.target.classList.remove('border-gray-700');
-
-        const productImage =document.getElementById('product-image');
-        productImage.src = '../images/teal.png'
-    }) 
+        //color add kortesi
+        event.target.classList.add("border-purple-600");
+        event.target.classList.remove("border-gray-300");
+        
+        const productImage = document.getElementById("product-image");
+        // productImage.src = "../images/gray.png";
+        // productImage.src = `../images/${color}.png`;
+        let productImageBase = "../images/";
+    productImage.src = productImageBase + color + ".png";
+  });
 }
 
+
+
+function selectWristSize(size){
+    const sizes = ['S', 'M', 'L', 'XL'];
+    for(let i = 0; i<sizes.length; i++){
+        const button = document.getElementById('size-'+ sizes[i]);
+           const element =sizes[i];
+          if(size===element){
+           button.classList.add("border-purple-600")
+          }else{
+            button.classList.remove('border-purple-600')
+          }
+    }
+}
