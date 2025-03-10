b95ca9bb692b19f98d4e2fd435a6892d6067d187
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-03-11 01:37:42 +0600

    add to caed section first part

--- js/script.js
+++ js/script.js
@@ -49,3 +49,20 @@ for(let btn of quantityElement){
         })  
 }
  
+// add to card 
+let cartCount = 0;
+document.getElementById('add-to-cart')
+.addEventListener('click', function(){
+  
+  const quantity = parseInt(document.getElementById('quantity').innerText);
+  
+  if(quantity > 0){
+
+     document.getElementById('checkout-container').classList.remove('hidden');
+     cartCount = cartCount +quantity;
+     document.getElementById('cart-count').innerText  = cartCount;
+ }
+ else{
+  alert('please add a quantity')
+ }
+})
